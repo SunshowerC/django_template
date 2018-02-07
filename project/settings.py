194571ac226ef11998d918ee064f2dc3dfe2bb86
willchen <1300546909@qq.com>
2018-02-07 11:07:23 +0800

    add static

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',   # 消息传递
     'django.contrib.staticfiles', # 静态文件
     'project.apps.polls',
+    'project.apps.teststatic',
 
 ]
 
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder', # 从安装的应用的static 目录查找
+]
