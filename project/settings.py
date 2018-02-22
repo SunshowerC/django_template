320bcf262a3d68edec102a3ee76b6f56cac0ab86
willchen <1300546909@qq.com>
2018-02-22 17:35:52 +0800

    change sqlite3 to mysql

--- project/settings.py
+++ project/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_web',
+        'USER': 'root',
+        'PASSWORD': 'bai123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
